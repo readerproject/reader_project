abdeab90892549c72757c7e4268f299904c8c993
MrXu <709332175@qq.com>
2019-11-22 11:47:45 +0800

    HeaderGuide-css

--- src/components/HeaderGuide/HeaderGuide.vue
+++ src/components/HeaderGuide/HeaderGuide.vue
@@ -1,13 +1,13 @@
 <template>
  <div class="tab" >
       <div class="tab-item">
-        <router-link to="/original">原创</router-link>
+        <router-link to="/original" ><span :class="{active:$route.path === '/original'}">原创</span ></router-link>
       </div>
       <div class="tab-item" >
-        <router-link to="/romance">言情</router-link>
+        <router-link to="/romance"><span :class="{active:$route.path === '/romance'}">言情</span></router-link>
       </div>
       <div class="tab-item" >
-        <router-link to="/tongren">同人</router-link>
+        <router-link to="/tongren"><span :class="{active:$route.path === '/tongren'}">同人</span></router-link>
       </div>
   </div>
 </template>
@@ -22,9 +22,19 @@
     width 100%
     height 50px
     display flex
+    position absolute
+    background: linear-gradient(to bottom, rgba(0,0,0,.4) 0%,rgba(0,0,0,0) 100%);
+    z-index 2
     .tab-item
-      margin 0 20px
+      margin 0 9px
+      padding 2px 0
       height 50px
       line-height 50px
- 
+
+      span 
+        color #fff
+        font-size 14px
+        font-weight bold
+        &.active
+          font-size 20px
 </style>
