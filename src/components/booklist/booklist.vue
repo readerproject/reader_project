d3fb0e3250213186bc23d94a6e4d3e4498496cea
runrunman <1103402143@qq.com>
2019-11-22 08:27:01 +0800

    romance

--- src/components/booklist/booklist.vue
+++ src/components/booklist/booklist.vue
@@ -1,44 +1,135 @@
 <template>
-  <div class="book_container">
-    <ul class="book_list">
-      <li class="book_li">
-        <a>
-          <div class="book_left">
+  <div class="book_list swiper-container2 ">
+    <div class="swiper-wrapper ">
+      <div class="swiper-slide">
+        <div class="book_left">
             <img class="book_img" src="../../common/image/swiper/153cc96455475f245fd0479c125ed1cf10e9.jpg" alt="">
           </div>
           <div class="book_right">
-            <section>
+            <section class="novelName">
               <h4>鬼斧</h4>
             </section>
-            <section>
+            <section class="novelIntroduce">
               <h5>我不打人，打神</h5>
             </section>
-            <section>
-              <section>
-                <h5></h5>
-              </section>
-              <section>
-                <div>玄幻</div>
-                <div>搞笑</div>
-              </section>
+            <section class="tagBox">
+              <p class="authorName">何所尽</p>
+              <p class="novelLabel">综合</p>
+              <p class="novelLabel">搞笑</p>
             </section>
           </div>
-        </a>
-      </li>
-    </ul>
+      </div>
+      <div class="swiper-slide">
+        <div class="book_left">
+          <img class="book_img" src="../../common/image/swiper/4f8027546d6a0e30fbe69f1b712e50e7.jpg" alt="">
+        </div>
+        <div class="book_right">
+          <section class="novelName">
+            <h4>鬼斧</h4>
+          </section>
+          <section class="novelIntroduce">
+            <h5>我不打人，打神</h5>
+          </section>
+          <section class="tagBox">
+            <p class="authorName">何所尽</p>
+            <p class="novelLabel">综合</p>
+            <p class="novelLabel">搞笑</p>
+          </section>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <div class="book_left">
+          <img class="book_img" src="../../common/image/swiper/6310ad634445991abffc1f88c62451c5.jpg" alt="">
+        </div>
+        <div class="book_right">
+          <section class="novelName">
+            <h4>鬼斧</h4>
+          </section>
+          <section class="novelIntroduce">
+            <h5>我不打人，打神</h5>
+          </section>
+          <section class="tagBox">
+            <p class="authorName">何所尽</p>
+            <p class="novelLabel">综合</p>
+            <p class="novelLabel">搞笑</p>
+          </section>
+        </div>`
+      </div>
+      
+    </div>
+    <div class="swiper-pagination"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Swiper from 'swiper'
+  import 'swiper/css/swiper.min.css'
   export default {
+    mounted(){
+    new Swiper('.swiper-container2',{
+      loop : true,
+      pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    
+  },
+    })
+  }
   }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
   @import "../../common/stylus/mixins.styl"
-  .book_container
-    .book_list
-      .book_li
+  .book_list
+    width 100%
+    height 158px
+    .swiper-pagination-bullets
+      position absolute
+      bottom 86px
+    .swiper-wrapper
+      width 100%
+      height 130px
+      margin 22px
+      display flex
+      .swiper-slide
+        display flex
+        width 240px
+        height 95px
+        
         bottom border -1px(#f1f1f1)
+        display flex
+        border-radius 10px
+        .book_left
+          img 
+            width 71px
+            height 95px
+            border-radius 10px 0 0 10px
+        .book_right
+          border-radius 10px
+          background #eee
+          width 229px
+          display flex
+          flex-direction: column
+          justify-content: space-around
+          padding  6px
+          .novelName
+            font-size 14px
+            color #333
+            font-family Arail
+            font-weight:bold
+          .novelIntroduce
+            font-size 12px
+          .tagBox
+            .authorName
+              float left
+              overflow hidden
+              font-size 12px
+            .novelLabel
+              float right
+              font-size 10px
+              background #F3F3F3
+              border-radius 30%
+              padding 8px
+              margin 2px
 
 </style>
--- src/pages/Romance/Romance.vue
+++ src/pages/Romance/Romance.vue
@@ -26,25 +26,68 @@
           <p></p>
         </div>
       </div>
-      <div class="dayrecBox">
-        <div class="indexModelTitle">
+      <div class="dayrecBox1">
+        <span class="indexModelTitle">
           <span class="titleCon">
             <img src="../../common/image/icon/small/title2.png" alt="">
           </span>
           <span>今日必读</span>
+          
           <booklist/>
-        </div>
-        <div></div>
-        <div></div>
-        <div></div>
+          
+        </span>
+        
       </div>
-      <div class="dayrecBox">
+      <div class="dayrecBox2">
         <div class="indexModelTitle">
           <span class="titleCon">
             <img src="../../common/image/icon/small/title2.png" alt="">
           </span>
           <span>本周强推</span>
         </div>
+        <div class="pushList">
+          <div class="ui-link">
+            <img src="../../common/image/item/153cb3a4bd6947b928a220e7eb18eaff9e9b.jpg" alt="">
+            <p class="novelName">我们好像在哪见过</p>
+            <p class="authorName">枣吞囵囫</p>
+          </div>
+          <div class="ui-link">
+            <img src="../../common/image/item/153cb3a4bd6947b928a220e7eb18eaff9e9b.jpg" alt="">
+            <p class="novelName">我们好像在哪见过</p>
+            <p class="authorName">枣吞囵囫</p>
+          </div>
+          <div class="ui-link">
+            <img src="../../common/image/item/180575c8985459251a2f32d961c7349e194efad.jpg" alt="">
+            <p class="novelName">我们好像在哪见过</p>
+            <p class="authorName">枣吞囵囫</p>
+          </div>
+          <div class="ui-link">
+            <img src="../../common/image/item/76be0bf82e4d729de228990020dee116.png" alt="">
+            <p class="novelName">我们好像在哪见过</p>
+            <p class="authorName">枣吞囵囫</p>
+          </div>
+          <div class="ui-link">
+            <img src="../../common/image/item/86142c5e0d4181cfa2f7918f2c2d7334703865.jpg" alt="">
+            <p class="novelName">我们好像在哪见过</p>
+            <p class="authorName">枣吞囵囫</p>
+          </div>
+          <div class="ui-link">
+            <img src="../../common/image/item/990121cd393d038f2aa2f47c5931d2a0ee80bd2.jpg" alt="">
+            <p class="novelName">我们好像在哪见过</p>
+            <p class="authorName">枣吞囵囫</p>
+          </div>
+          <div class="ui-link">
+            <img src="../../common/image/item/a32a6e528f13ef5efd331508211e2b67.jpg" alt="">
+            <p class="novelName">我们好像在哪见过</p>
+            <p class="authorName">枣吞囵囫</p>
+          </div>
+          <div class="ui-link">
+            <img src="../../common/image/item/a984eb1f52c4c20de28e8c7fa9b09cf8.png" alt="">
+            <p class="novelName">我们好像在哪见过</p>
+            <p class="authorName">枣吞囵囫</p>
+          </div>
+        </div>
+        
       </div>
   </section>
 
@@ -53,7 +96,6 @@
 <script type="text/ecmascript-6">
   import Swiper from 'swiper'
   import 'swiper/css/swiper.min.css'
-  // import {mapState} from 'vuex'
   import classify from '../../components/classify/classify'
   import booklist from '../../components/booklist/booklist'
   export default {
@@ -65,7 +107,8 @@
       this.$nextTick(() => {
         new Swiper('.swiper-container',{
         pagination: {
-          el: '.swiper-pagination'
+          el: '.swiper-pagination',
+          
         },
         loop:true,
         autoplay:true
@@ -101,24 +144,58 @@
       img 
         width 28px
         height 28px
-    .dayrecBox
+    .dayrecBox1
       width 100%
-      height 185px
+      height 158px
       .indexModelTitle
         width 100%
-        height 58px
         color #333
         font-size 17px
-        padding 18px 16px
+        margin 18px 16px
         .titleCon
           img 
             width 11px
             height 23px
             vertical-align middle
-      
+      .pushList
+        width 100%
+        height 297px 
+        background pink 
+        box-sizing border-box
+        padding 14px
+    .dayrecBox2
+      width 100%
+      height 386px
+      .indexModelTitle
+        width 100%
+        color #333
+        font-size 17px
+        margin 18px 16px
+        .titleCon
+          img 
+            width 11px
+            height 23px
+            vertical-align middle
+      .pushList
+        width 100%
+        height 297px 
         
- 
-      
-      
-      
+        box-sizing border-box
+        padding 0 14px
+        display flex
+        justify-content space-between
+        flex-wrap wrap
+        .ui-link
+          width 70px
+          height 155px
+          img 
+            width 70px
+            height 93px
+            
+          .novelName
+            font-size 13px
+            margin 6px 0
+          .authorName
+            font-size 13px
+            color #8F8F9E
 </style>
