bca1251a3faaf1ddc9b3ff482a1af521de2ce811
wwg <a1034987437@163.com>
2019-11-22 15:57:19 +0800

    tongren-pages have done

--- src/pages/Tongren/Tongren.vue
+++ src/pages/Tongren/Tongren.vue
@@ -17,16 +17,186 @@
     <div class="strongPush">
       <div class="indexModelTitle">
         <span class="titleCon"></span>
-        <span>本周强推</span>
+        <span class="block-item">本周强推</span>
       </div>
-      <ul></ul>
+      <ul class="modelBox_4 pushList">
+        <li>
+          <a href="#" class="ui-link">
+            <img src="../../common/images/item/153cb3a4bd6947b928a220e7eb18eaff9e9b.jpg" alt="" class="novelImg">
+            <p class="novelName">【DC】真香（ABO,老爷阿卡姆白月光）</p>
+            <p class="authorName">乔恩颂</p>
+          </a>
+        </li>
+         <li>
+          <a href="#" class="ui-link">
+            <img src="../../common/images/item/153cb3a4bd6947b928a220e7eb18eaff9e9b.jpg" alt="" class="novelImg">
+            <p class="novelName">【DC】真香（ABO,老爷阿卡姆白月光）</p>
+            <p class="authorName">乔恩颂</p>
+          </a>
+        </li>
+         <li>
+          <a href="#" class="ui-link">
+            <img src="../../common/images/item/153cb3a4bd6947b928a220e7eb18eaff9e9b.jpg" alt="" class="novelImg">
+            <p class="novelName">【DC】真香（ABO,老爷阿卡姆白月光）</p>
+            <p class="authorName">乔恩颂</p>
+          </a>
+        </li>
+         <li>
+          <a href="#" class="ui-link">
+            <img src="../../common/images/item/153cb3a4bd6947b928a220e7eb18eaff9e9b.jpg" alt="" class="novelImg">
+            <p class="novelName">【DC】真香（ABO,老爷阿卡姆白月光）</p>
+            <p class="authorName">乔恩颂</p>
+          </a>
+        </li> 
+        <li>
+          <a href="#" class="ui-link">
+            <img src="../../common/images/item/153cb3a4bd6947b928a220e7eb18eaff9e9b.jpg" alt="" class="novelImg">
+            <p class="novelName">【DC】真香（ABO,老爷阿卡姆白月光）</p>
+            <p class="authorName">乔恩颂</p>
+          </a>
+        </li> 
+        <li>
+          <a href="#" class="ui-link">
+            <img src="../../common/images/item/153cb3a4bd6947b928a220e7eb18eaff9e9b.jpg" alt="" class="novelImg">
+            <p class="novelName">【DC】真香（ABO,老爷阿卡姆白月光）</p>
+            <p class="authorName">乔恩颂</p>
+          </a>
+        </li> 
+        <li>
+          <a href="#" class="ui-link">
+            <img src="../../common/images/item/153cb3a4bd6947b928a220e7eb18eaff9e9b.jpg" alt="" class="novelImg">
+            <p class="novelName">【DC】真香（ABO,老爷阿卡姆白月光）</p>
+            <p class="authorName">乔恩颂</p>
+          </a>
+        </li>
+         <li>
+          <a href="#" class="ui-link">
+            <img src="../../common/images/item/153cb3a4bd6947b928a220e7eb18eaff9e9b.jpg" alt="" class="novelImg">
+            <p class="novelName">【DC】真香（ABO,老爷阿卡姆白月光）</p>
+            <p class="authorName">乔恩颂</p>
+          </a>
+        </li> 
+        
+      </ul>
     </div>
     <p class="line"></p>
+    <!-- 新书精选 -->
+    <div class="newBook">
+      <div class="indexModelTitle">
+        <span class="titleCon"></span>
+        <span class="block-item">新书精选</span>
+      </div>
+      <div class="strongPushList">
+        <div class="swiper-container-newBook">
+          <ul class="swiper-wrapper">
+            <li class="swiper-slide"><a class="ui-link" href="#"><img src="../../common/images/swiper/shaonian.jpg" alt="1"></a></li>
+            <li class="swiper-slide"><a class="ui-link" href="#"><img src="../../common/images/swiper/shaonian.jpg" alt="1"></a></li>
+            <li class="swiper-slide"><a class="ui-link" href="#"><img src="../../common/images/swiper/shaonian.jpg" alt="1"></a></li>
+            <li class="swiper-slide"><a class="ui-link" href="#"><img src="../../common/images/swiper/shaonian.jpg" alt="1"></a></li>
+            <li class="swiper-slide"><a class="ui-link" href="#"><img src="../../common/images/swiper/shaonian.jpg" alt="1"></a></li>
+            <li class="swiper-slide"><a class="ui-link" href="#"><img src="../../common/images/swiper/shaonian.jpg" alt="1"></a></li>
+            <li class="swiper-slide"><a class="ui-link" href="#"><img src="../../common/images/swiper/shaonian.jpg" alt="1"></a></li>
+            <li class="swiper-slide"><a class="ui-link" href="#"><img src="../../common/images/swiper/shaonian.jpg" alt="1"></a></li>
+            <li class="swiper-slide"><a class="ui-link" href="#"><img src="../../common/images/swiper/shaonian.jpg" alt="1"></a></li>
+          </ul> 
+        </div> 
+      </div> 
+    </div>
+    <p class="line"></p>
+
+
+    <!-- 完结推荐 -->
+    <div class="newBook">
+      <div class="indexModelTitle">
+        <span class="titleCon"></span>
+        <span class="block-item">完结推荐</span>
+      </div>
+      <ul class="strongPushList">
+        <li class="novelModel">
+          <a class="ui-link">
+            <img src="../../common/images/swiper/shaonian.jpg" alt="" class="novelImg">
+            <div class="novelRightBox">
+              <p class="novelName">[恋与制作人]二位一体</p>
+              <p class="novelIntroduce">白起是我大伯哥，悠然是我主人格</p>
+              <div class="mark">
+                <span class="novelAuthor">丰年虫</span>
+                <span class="novelLabel read">20.2万</span>
+                <span class="novelLabel grayTag">国产</span>
+                <span class="novelLabel grayTag">甜宠</span>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="novelModel">
+          <a class="ui-link">
+            <img src="../../common/images/swiper/shaonian.jpg" alt="" class="novelImg">
+            <div class="novelRightBox">
+              <p class="novelName">[恋与制作人]二位一体</p>
+              <p class="novelIntroduce">白起是我大伯哥，悠然是我主人格</p>
+              <div class="mark">
+                <span class="novelAuthor">丰年虫</span>
+                <span class="novelLabel read">20.2万</span>
+                <span class="novelLabel grayTag">国产</span>
+                <span class="novelLabel grayTag">甜宠</span>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="novelModel">
+          <a class="ui-link">
+            <img src="../../common/images/swiper/shaonian.jpg" alt="" class="novelImg">
+            <div class="novelRightBox">
+              <p class="novelName">[恋与制作人]二位一体</p>
+              <p class="novelIntroduce">白起是我大伯哥，悠然是我主人格</p>
+              <div class="mark">
+                <span class="novelAuthor">丰年虫</span>
+                <span class="novelLabel read">20.2万</span>
+                <span class="novelLabel grayTag">国产</span>
+                <span class="novelLabel grayTag">甜宠</span>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="novelModel">
+          <a class="ui-link">
+            <img src="../../common/images/swiper/shaonian.jpg" alt="" class="novelImg">
+            <div class="novelRightBox">
+              <p class="novelName">[恋与制作人]二位一体</p>
+              <p class="novelIntroduce">白起是我大伯哥，悠然是我主人格</p>
+              <div class="mark">
+                <span class="novelAuthor">丰年虫</span>
+                <span class="novelLabel read">20.2万</span>
+                <span class="novelLabel grayTag">国产</span>
+                <span class="novelLabel grayTag">甜宠</span>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li class="novelModel">
+          <a class="ui-link">
+            <img src="../../common/images/swiper/shaonian.jpg" alt="" class="novelImg">
+            <div class="novelRightBox">
+              <p class="novelName">[恋与制作人]二位一体</p>
+              <p class="novelIntroduce">白起是我大伯哥，悠然是我主人格</p>
+              <div class="mark">
+                <span class="novelAuthor">丰年虫</span>
+                <span class="novelLabel read">20.2万</span>
+                <span class="novelLabel grayTag">国产</span>
+                <span class="novelLabel grayTag">甜宠</span>
+              </div>
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+
+
+    <!-- 完结推荐 -->
     <div class="newBook"></div>
     <p class="line"></p>
-    <div class="newBook"></div>
-    <p class="line"></p>
+    <!--人气推荐 -->
     <div class="vipChange"></div>
+    <!-- 底部 -->
     <div id="footer"></div>
   </div>
 </template>
@@ -42,7 +212,12 @@
           new Swiper('.swiper-container', {
             autoplay:true,
             loop: true,
+        }),
+        new Swiper('.swiper-container-newBook', {
+            slidesPerView : 4,
+            centeredSlides :false
         })
+       
       })
     }
   }
@@ -82,7 +257,7 @@
   //本周强推
   .strongPush            
     width 100%
-    height 100%
+    //height 100%
     .indexModelTitle
       box-sizing border-box
       padding 18px 16px
@@ -94,7 +269,155 @@
         float left 
         background url(https://m.gongzicp.com/h5/img/title.png) 0 0 no-repeat
         background-size 100% 100%
-        span 
-          font-size 22px 
-  
+      .block-item 
+        line-height 22px
+    .modelBox_4
+      display flex
+      flex-wrap wrap
+      justify-content space-between
+      margin 0 16px  
+      li
+        display  flex
+        width 70px
+        margin 0 0 19px
+        .ui-link
+          width  100%
+          .novelImg
+            display block
+            width 100%
+            height 93px
+            border-radius 4px
+          .novelName
+            color black
+            font-size 12px
+            line-height 18px
+            max-height  72px
+            display -webkit-box
+            -webkit-box-orient vertical
+            -webkit-line-clamp 2
+            overflow hidden
+          .authorName
+            font-size 12px
+            height 24px
+            line-height 18px
+            color #8f8f8f
+  //新书
+  .newBook
+    width 100%
+    .indexModelTitle
+      box-sizing border-box
+      padding 18px 16px
+      width 100%
+      height 58px 
+      .titleCon
+        width 11px
+        height 23px
+        float left 
+        background url(https://m.gongzicp.com/h5/img/title.png) 0 0 no-repeat
+        background-size 100% 100%
+      .block-item 
+        line-height 22px
+    .strongPushList
+      width 100%
+      box-sizing border-box
+      padding 0 16px 16px 
+      .swiper-container-newBook
+        margin 0 auto 
+        //position relative
+        overflow hidden
+        list-style none 
+        .swiper-wrapper
+          width 100%
+          height 100%
+          display flex
+          // flex-wrap nowrap
+          justify-content space-between
+          //position relative
+          .swiper-slide
+            width  25% 
+            height 100%
+            a 
+              height 100%
+              width 100%
+              img
+                float left
+                width 70px
+                height 93px
+  //完结
+  .newBook
+    width 100%
+    .indexModelTitle
+      box-sizing border-box
+      padding 18px 16px
+      width 100%
+      height 58px 
+      .titleCon
+        width 11px
+        height 23px
+        float left 
+        background url(https://m.gongzicp.com/h5/img/title.png) 0 0 no-repeat
+        background-size 100% 100%
+      .block-item 
+        line-height 22px
+    .strongPushList
+      width 100%
+      box-sizing border-box
+      padding 0 16px 16px 
+      .novelModel
+        width 100%
+        height 100px
+        box-sizing border-box
+        margin 0 0 23px
+        .ui-link
+          width 100%
+          .novelImg
+            float left
+            display block
+            width 70px
+            height 93px
+            border-radius 4px
+          .novelRightBox
+            float left
+            width 260px
+            padding 2px 0 0 11px 
+            .novelName
+              margin 8px 0 0
+              color #000
+              height 22px
+              overflow hidden
+            .novelIntroduce
+              margin 6px 0 
+              height 38px
+              font-size 14px
+              display -webkit-box
+              -webkit-box-orient vertical
+              -webkit-line-clamp 2
+              overflow hidden
+            .mark
+              width 100%
+              .novelAuthor
+                float left
+              .novelLabel
+                float right
+                height 20px
+                font-size 12px
+                box-sizing border-box
+                margin 0 2px
+                padding 2px 8px
+                white-space nowrap 
+                text-overflow ellipsis
+                overflow hidden
+                text-align center
+                border-radius 10px
+                border 1px solid #999
+              .novelAuthor
+                font-size 14px
+              .grayTag
+                color #8d8d8d
+                background rgba(211,211,211,0.2)
+                border none
+
+
+
+
 </style>
